72d575685dde80ad0984240a8730ea295cdfcd58
bodoralex <bodaleaa@gmail.com>
2017-01-25 15:22:01 +0100

    initial commit

---
+++ src/Logger.java
@@ -0,0 +1,5 @@
+/**
+ * Created by bodor on 2017.01.25..
+ */
+public class Logger {
+}
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Created by bodor on 2017.01.25..
+ */
+public class Main {
+}
+
+
+
+    /*public static void main(String[] args) {
+        int[][] sajt = generateSimulation(100);
+        String nline = "";
+        for (int[] a : sajt) {
+            for (int b : a) {
+                nline += b + ",";
+            }
+            System.out.println(nline.substring(0,nline.length() - 1));
+            nline = "";
+        }
+
+    }*/
