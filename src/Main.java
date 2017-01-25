d7fb74344d0fe638b82ae1fa363fdd66206bcd85
bodoralex <bodaleaa@gmail.com>
2017-01-25 13:33:16 +0100

    main skeleton

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Created by bodor on 2017.01.25..
+ */
+public class Main {
+
+
+    public static Simulation generateSimulation(int round){
+        //generateSimulation(int round), returns a
+        //Simulation instance that contains the simulation result
+        return Simulation;
+    }
+    public static void main(String[] args){
+
+
+    }
+}
