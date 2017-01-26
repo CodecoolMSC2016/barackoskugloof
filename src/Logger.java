6997414de369f207571e8206f818c4129d55074f
misomarcell <misomarcell@gmail.com>
2017-01-26 10:57:39 +0100

    kuglófbros

--- src/Logger.java
+++ src/Logger.java
@@ -1,5 +1,35 @@
-/**
- * Created by bodor on 2017.01.25..
- */
-public class Logger {
+import java.util.*;
+import java.text.*;
+import java.time.*;
+public class Logger
+{
+
+  String type;
+  String text;
+
+  public Logger(String inType, String inText)
+  {
+    type = inType;
+    text = inText;
+  }
+
+  public static void main(String[] args)
+  {
+    log("TimeStamp", "Segg");
+  }
+
+  public static void log(String type, String text)
+  {
+      DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+      Date date = new Date();
+      System.out.println("[" + dateFormat.format(date) + "] " + type + " - " + text);
+  }
+
+  public static String dateTime()
+  {
+    DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    Date date = new Date();
+
+    return dateFormat.format(date);
+  }
 }
